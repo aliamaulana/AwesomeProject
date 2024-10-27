1009cb1f448c17ec493feee7672ac1b2b25a3450
aliamaulana <aliaresti11@gmail.com>
2024-10-27 21:19:23 +0700

    acara 8

--- App.tsx
+++ App.tsx
@@ -76,17 +76,16 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="Nama">
+            Alia Maulana Resti
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
+          <Section title="NIM">
+            22/492734/SV/20607
           </Section>
-          <Section title="Debug">
-            <DebugInstructions />
+          <Section title="Kelas">
+            PGPBL A
           </Section>
-          <Section title="Learn More">
+          <Section title="Link Media Sosial">
             Read the docs to discover what to do next:
           </Section>
           <LearnMoreLinks />
